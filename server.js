39499a4bcb0f6aaf1264bf64c0e137c768ea9371
Joseph S Gansukh <g.sukhbat@gmail.com>
2020-12-20 11:55:16 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const express = require('express')
+const morgan = require('morgan')
 const { animals } = require('./data/animals.json')
 
 const app = express()
+app.use(morgan('dev'))
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = []
+  let filteredResults = animalsArray;  
+
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+
+    personalityTraitsArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    })
+  }
+
+  if(query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
 
 app.get('/api/animals', (req, res) => {
-  res.json(animals)
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results)
+  }
+  res.json(results)
 })
 
 app.listen(3001, () => {
